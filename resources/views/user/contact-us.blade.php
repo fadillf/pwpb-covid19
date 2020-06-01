acb2d57a24d8c500293e811398e8da7b2fdefb2c
fadillf <fadillahfadlurrahman@gmail.com>
2020-06-01 21:31:46 +0700

    Backend Contact Us

--- resources/views/user/contact-us.blade.php
+++ resources/views/user/contact-us.blade.php
@@ -2,20 +2,24 @@
 
 @section('content')
 <section id="contact">
-    <div class="container">
+    <div class="container mt-2">
         <h3 class="text-center text-uppercase">contact us</h3><hr>
         <p class="text-center w-75 m-auto">Jika anda memiliki saran dan masukkan terhadap aplikasi kami, silahkan kontak kami dengan mengisi kotak saran atau bisa menghubungi kami.</p>
+        @if(Session::has('status'))
+            <div class="alert alert-success">{{ Session::get('status') }}</div>
+        @endif
         <div class="row mt-4 mb-5">
         	<div class="col-sm-6 bg-white p-3 shadow-sm rounded mt-4 " style="margin: auto;">
-        		<form id="contact-form" action="#" method="get">
+        		<form id="contact-form" action="" method="POST">
+                    {{ csrf_field() }}
                      <div class="from-group mb-4 mt-3">
-                     	<input type="text" name="" class="form-control" placeholder="Your Name">
+                     	<input type="text" name="nama" class="form-control" placeholder="Your Name">
                      </div>
                      <div class="from-group mb-4">
-                     	<input type="text" name="" class="form-control" placeholder="Email">
+                     	<input type="email" name="email" class="form-control" placeholder="Email">
                      </div>
                      <div class="from-group mb-4">
-                     	<textarea class="form-control" style="resize: none;" rows="6" placeholder="Message"></textarea>
+                     	<textarea type="text" name="message" class="form-control" style="resize: none;" rows="6" placeholder="Message"></textarea>
                      </div>
                      <button class="btn btn-success">Send</button>
                 </form>
